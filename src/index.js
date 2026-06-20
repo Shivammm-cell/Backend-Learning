bbd0037af232606258e03082df70ffc85353c3af
Shivam Thakur <Shivamthakur49554@gmail.com>
2026-06-20 14:27:44 +0530

    utlis code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+           
+     app.on("error",(error) => {
+           console.log("ERROR: ", error)
+           throw error
+       })
+
+
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDb connection Failed!!!" , error)     
+})
 
 
 
