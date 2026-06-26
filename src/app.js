825820a9264cbba95472502ded3a3ea7500e0843
Shivam Thakur <Shivamthakur49554@gmail.com>
2026-06-26 16:44:39 +0530

    all setup configured (usercontroller, userrouter )

--- src/app.js
+++ src/app.js
@@ -18,7 +18,16 @@ app.use(cookieParser())
 
 
 app.use(express.json({limit:"16kb"}))
-export {app}
+
+//routes import 
+
+import userRouter from './routes/user.routes.js'
+
+//routes declaration 
+app.use("/api/v1/users" , userRouter)
+
+
+export  {app}
 
 
 
