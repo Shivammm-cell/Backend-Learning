3db1126d4c6f574bf64daff433a2a9b9ac0dc68c
Shivam Thakur <Shivamthakur49554@gmail.com>
2026-06-27 20:41:25 +0530

    debug(not mention await at createdUser

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,11 +17,11 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "all fields are required")
     }
 
-    const existedUser = User.findOne({
+    const existedUser =  await User.findOne({
         $or: [{ username }, { email }]
     })
 
-    if (existedUser) {
+    if (!existedUser) {
         throw new ApiError(409, "User with email or username already existed")
     }
 
@@ -39,7 +39,8 @@ const registerUser = asyncHandler(async (req, res) => {
     if (!avatar) {
         throw new ApiError(400, "avatar is required");
     }
-
+       
+    //saving user in database
     const user = await User.create({
         fullName,
         avatar: avatar.url,
@@ -48,9 +49,9 @@ const registerUser = asyncHandler(async (req, res) => {
         password,
         username: username.toLowerCase(),
     });
-
+          //fetching saved user form db
     const createdUser = await User.findById(user._id).select(
-        "-password -refreshToken"
+        "-password -refreshToken"        //removing password and refreshtoken form response 
     )
     if (!createdUser) {
         throw new ApiError(500, "something went wrong while registring a user ")
